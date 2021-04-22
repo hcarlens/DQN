e8581891e53278ffa82ad6f63854c4e83c632832
Harald <hcarlens@gmail.com>
2021-04-22 15:45:36 +0100

    Add test for sanity env v5

--- run_training.py
+++ run_training.py
@@ -9,24 +9,26 @@ from pytorch_rl.dqn_agent import DQNAgent
 from pytorch_rl.agent_trainer import Trainer
 from pytorch_rl.memory_buffer import MemoryBuffer
 from pytorch_rl.utils import loss_functions, optimisers, create_sequential_model
+from pytorch_rl.sanity_envs import SanityEnvV4, SanityEnvV5
 import argparse
 
 
 parser = argparse.ArgumentParser(description='Train a DQN agent on CartPole.')
 parser.add_argument('--seed',  type=int, default=None, help='Random seed. ')
-parser.add_argument('--num_episodes',  type=int, default=500, help='Number of episodes to train for. ')
+parser.add_argument('--num_episodes',  type=int, default=5000, help='Number of episodes to train for. ')
 parser.add_argument('--loss_fn',  type=str, default='mse', help='Loss function. ')
 parser.add_argument('--optimiser',  type=str, default='adam', help='Loss function. ')
-parser.add_argument('--lr',  type=float, default=0.00025, help='Learning rate. ')
+parser.add_argument('--lr',  type=float, default=0.001, help='Learning rate. ')
 parser.add_argument('--discount',  type=float, default=0.99, help='Discount rate. ')
-parser.add_argument('--batch_size',  type=int, default=128, help='Batch size. ')
-parser.add_argument('--layers_spec',  type=str, default='16', help='Spec for the main net. E.g. "32_32" for two layers with 32 neurons. ')
-parser.add_argument('--final_layer_neurons',  type=int, default=16, help='Number of neurons in the final layer. ')
-parser.add_argument('--buffer_length',  type=int, default=50000, help='Maximum number of memories stored in the memory buffer before overwriting. ')
-parser.add_argument('--timestep_to_start_learning',  type=int, default=1000, help='Timestep at which we start updating the agent. ')
-parser.add_argument('--target_update_steps',  type=int, default=300, help='Frequency at which to update the target network. ')
-parser.add_argument('--test_every_n_steps',  type=int, default=5000, help='Frequency at which to run a test episode. ')
-parser.add_argument('--epsilon_decay_rate',  type=float, default=0.99, help='Rate at which epsilon (random action probability) decays. ')
+parser.add_argument('--batch_size',  type=int, default=16, help='Batch size. ')
+parser.add_argument('--layers_spec',  type=str, default='64', help='Spec for the main net. E.g. "32_32" for two layers with 32 neurons. ')
+parser.add_argument('--final_layer_neurons',  type=int, default=64, help='Number of neurons in the final layer. ')
+parser.add_argument('--buffer_length',  type=int, default=100, help='Maximum number of memories stored in the memory buffer before overwriting. ')
+parser.add_argument('--timestep_to_start_learning',  type=int, default=20, help='Timestep at which we start updating the agent. ')
+parser.add_argument('--target_update_steps',  type=int, default=10, help='Frequency at which to update the target network. ')
+parser.add_argument('--test_every_n_steps',  type=int, default=20, help='Frequency at which to run a test episode. ')
+parser.add_argument('--train_every_n_steps',  type=int, default=1, help='Frequency at which to run a backward pass. ')
+parser.add_argument('--epsilon_decay_rate',  type=float, default=0.5, help='Rate at which epsilon (random action probability) decays. ')
 parser.add_argument('--cuda',  action='store_true', default=False, help='Pass this flag to train on GPU instead of CPU. ')
 parser.add_argument('--gymenv',  type=str, default='CartPole-v1', help='Choose an OpenAI Gym env to run the agent on. ')
 
@@ -73,6 +75,7 @@ trainer = Trainer(
     target_update_steps=args.target_update_steps,
     epsilon_decay_rate=args.epsilon_decay_rate,
     random_seed=args.seed,
+    train_every_n_steps=args.train_every_n_steps,
     test_every_n_steps=args.test_every_n_steps,
     hparams={'lr': args.lr, 'dr': args.discount, 'layers': args.layers_spec, 'final_layer': args.final_layer_neurons,
              'cuda': args.cuda, 'optimiser': args.optimiser, 'loss_fn': args.loss_fn, 'buffer_length': args.buffer_length,
--- tests/test_sanity_envs.py
+++ tests/test_sanity_envs.py
@@ -1,7 +1,8 @@
 from pytorch_rl.dqn_agent import DQNAgent
 from pytorch_rl.agent_trainer import Trainer
 from pytorch_rl.sanity_envs import (SanityEnvV0, SanityEnvV1, SanityEnvV2,
-                                                  SanityEnvV3, SanityEnvV4)
+                                                  SanityEnvV3, SanityEnvV4,
+                                    SanityEnvV5)
 from pytorch_rl.memory_buffer import MemoryBuffer
 from pytorch_rl.utils import loss_functions, optimisers, create_sequential_model
 import torch
@@ -207,5 +208,42 @@ def test_sanity_env_v4():
         write_to_tensorboard=False
     )
     trainer.run(num_episodes=300)
+
+    assert trainer.loss_values.max() < 1e-5, 'Loss is too high on sanity env 4'
+
+def test_sanity_env_v5():
+    seed_everything()
+    loss_fn = loss_functions['mse']
+    optimiser = optimisers['adam']
+    env = SanityEnvV5()
+    dqn_net = create_sequential_model(num_inputs=env.observation_space.shape[0], layers_spec=(64,), num_outputs=64,
+                                      dropout_rate=0, activation_function='relu', final_activation=True)
+
+    dqn_agent = DQNAgent(learning_rate=1e-2,
+                        discount_rate=0.99,
+                        main_net=dqn_net,
+                        final_layer_neurons=64,
+                        num_outputs=env.action_space.n,
+                        random_seed=RANDOM_SEED,
+                        loss_fn=loss_fn,
+                        optimiser=optimiser,
+                        cuda=False,
+                        )
+
+    trainer = Trainer(
+        env=env,
+        agent=dqn_agent,
+        memory_buffer=MemoryBuffer(buffer_length=100),
+        start_epsilon=1,
+        timestep_to_start_learning=20,
+        batch_size=16,
+        target_update_steps=10,
+        epsilon_decay_rate=0.5,
+        random_seed=RANDOM_SEED,
+        max_num_steps=1000000,
+        train_every_n_steps=1,
+        write_to_tensorboard=False
+    )
+    trainer.run(num_episodes=500)
 
     assert trainer.loss_values.max() < 1e-5, 'Loss is too high on sanity env 4'
